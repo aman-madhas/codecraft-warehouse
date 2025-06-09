a7e0f872b387bd658bcc1c142cdd7c57122d2a3e
Amandeep Madhas <amadhas@idbs.com>
2025-06-09 16:11:38 +0100

    added Customer.charged()

---
+++ src/main/java/com/Customer.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Customer {
+
+    boolean charged() {
+        return true;
+    }
+}
--- src/test/java/com/CustomerTest.java
+++ src/test/java/com/CustomerTest.java
@@ -2,10 +2,13 @@ package com;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerTest {
 
     @Test
     void successfullyBuyCD() {
-
+        Customer customer = new Customer();
+        assertEquals(true, customer.charged());
     }
 }
